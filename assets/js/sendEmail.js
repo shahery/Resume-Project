3639ad9c0551872b52d3f01422dd059506502d63
shahery <shaheryar924@gmail.com>
2022-05-01 01:32:01 +0000

    Add correction in contact.html and sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("service_9qdy45b", "shaher", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
